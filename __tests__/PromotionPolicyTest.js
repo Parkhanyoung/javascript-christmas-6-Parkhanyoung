b38d590f51bc3217a205f12d8f70e5b7314f48cf
hanyoung <phanyoung@naver.com>
2023-11-10 17:30:13 +0900

    test: 데일리(평일/주말) 할인액 계산 기능 테스트 추가

--- __tests__/PromotionPolicyTest.js
+++ __tests__/PromotionPolicyTest.js
@@ -1,13 +1,27 @@
 import ChristmasDdayDiscount from "../src/domain/PromotionPolicy/ChritmasDdayDiscount.js";
+import DailyDiscount from "../src/domain/PromotionPolicy/DailyDiscount.js";
 
 describe("이벤트 정책에 대한 테스트", () => {
-  describe("Chritmas D-day 정책에 대한 테스트", () => {
+  describe("Chritmas D-day 할인 정책에 대한 테스트", () => {
     test.each([
       [new Date("2023-12-06"), 1500],
       [new Date("2023-12-12"), 2100],
       [new Date("2023-12-24"), 3300],
-    ])("날짜에 맞게 알맞는 할인 금액을 반환한다. %s - %s", (date, discountAmount) => {
+    ])("날짜에 맞게 할인 금액을 계산한다. %s - %s", (date, discountAmount) => {
       expect(ChristmasDdayDiscount.apply(date)).toBe(discountAmount);
     });
+  });
+
+  describe("Daily(평일/주말) 할인 정책에 대한 테스트", () => {
+    test.each([
+      [new Date("2023-12-10"), { dessert: 3, main: 2 }, 6069],
+      [new Date("2023-12-16"), { dessert: 5, main: 2 }, 4046],
+      [new Date("2023-12-25"), { dessert: 1, main: 4 }, 2023],
+    ])(
+      "날짜와 주문한 메뉴 정보에 맞게 할인 금액을 계산한다. %s, %s - %s원",
+      (date, menu, discountAmount) => {
+        expect(DailyDiscount.apply(date, menu)).toBe(discountAmount);
+      }
+    );
   });
 });

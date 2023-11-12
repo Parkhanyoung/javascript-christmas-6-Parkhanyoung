ccf535e65eafdd42ed5cd79ac59fe3b449b9ce8f
hanyoung <phanyoung@naver.com>
2023-11-12 13:51:29 +0900

    feat: 주문 입력값 파싱

---
+++ src/domain/OrdersParser.js
@@ -0,0 +1,22 @@
+import { ERROR_MESSAGE } from "../constants/messages.js";
+
+const OrdersParser = {
+  parse(value) {
+    this.validateIsString(value);
+    const parsed = this.splitByComma(value);
+
+    return parsed;
+  },
+
+  validateIsString(value) {
+    if (typeof value !== "string") {
+      throw Error(ERROR_MESSAGE.invalidOrder);
+    }
+  },
+
+  splitByComma(input) {
+    return input.split(",");
+  },
+};
+
+export default OrdersParser;

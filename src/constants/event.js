b2e60254fb3eeb55e8cff341ea42c1684debb559
hanyoung <phanyoung@naver.com>
2023-11-13 12:09:27 +0900

    feat: 이벤트 적용 내역 출력

---
+++ src/constants/event.js
@@ -0,0 +1,9 @@
+const EVENT = {
+  christmasDdayDiscount: "크리스마스 디데이 할인",
+  weekdayDiscount: "평일 할인",
+  weekendDiscount: "주말 할인",
+  specialDiscount: "특별 할인",
+  gift: "증정 이벤트",
+};
+
+export { EVENT };
--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,11 +1,93 @@
 import { Console } from "@woowacourse/mission-utils";
+import { EVENT } from "../constants/event.js";
 
 const OutputView = {
-  printMenu() {
-    Console.print("<주문 메뉴>");
-    // ...
+  printBlankLine() {
+    Console.print("");
   },
-  // ...
+
+  printNull() {
+    Console.print("없음");
+    this.printBlankLine();
+  },
+
+  printIntro() {
+    Console.print("12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+    this.printBlankLine();
+  },
+
+  printMenu(menu) {
+    Console.print("<주문 메뉴>");
+
+    if (!menu) {
+      return this.printNull();
+    }
+
+    const menus = Object.entries(menu);
+    const menuStrings = menus.map(([name, count]) => `${name} ${count}개`);
+
+    menuStrings.forEach((string) => Console.print(string));
+    this.printBlankLine();
+  },
+
+  printAmountBeforeDiscount(amount) {
+    Console.print("<할인 전 총주문 금액>");
+    Console.print(`${amount.toLocaleString()}원`);
+    this.printBlankLine();
+  },
+
+  printGift(gift) {
+    Console.print("<증정 메뉴>");
+
+    if (!gift) {
+      return this.printNull();
+    }
+
+    const [name, count] = Object.entries(gift)[0];
+
+    Console.print(`${name} ${count}개`);
+    this.printBlankLine();
+  },
+
+  printAppliedBenefit(appliedBenefit) {
+    Console.print("<혜택 내역>");
+
+    if (!appliedBenefit) {
+      return this.printNull();
+    }
+
+    const benefits = Object.entries(appliedBenefit);
+    const benefitStrings = benefits.map(
+      ([benefitType, amount]) => `${EVENT[benefitType]}: -${amount.toLocaleString()}원`
+    );
+
+    benefitStrings.forEach((string) => Console.print(string));
+
+    this.printBlankLine();
+  },
+
+  printBenefitAmount(benefitAmount) {
+    Console.print("<총혜택 금액>");
+    Console.print(`-${benefitAmount.toLocaleString()}원`);
+    this.printBlankLine();
+  },
+
+  printAmountAfterDiscount(amount) {
+    Console.print("<할인 후 예상 결제 금액>");
+    Console.print(`${amount.toLocaleString()}원`);
+    this.printBlankLine();
+  },
+
+  printBadge(badge) {
+    Console.print("<12월 이벤트 배지>");
+
+    if (!badge) {
+      return this.printNull();
+    }
+
+    Console.print(badge);
+    this.printBlankLine();
+  },
 };
 
 export default OutputView;
